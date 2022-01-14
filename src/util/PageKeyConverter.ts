961f612908f1d04c56a369b3bad62a484432b6e7
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2022-01-15 00:15:29 +0200

    fix: handle `PageKeyConverter` edge-cases

--- src/util/PageKeyConverter.ts
+++ src/util/PageKeyConverter.ts
@@ -5,10 +5,20 @@ export class PageKeyConverter {
   constructor(public prefix: string) {}
 
   toKey(index: number): string {
+    if (index < 0) throw new RangeError("Index must be zero or greater.");
     return `${this.prefix}${index}`;
   }
 
   toIndex(key: string): number {
-    return parseInt(key.replace(this.prefix, ""));
+    if (!key.startsWith(this.prefix))
+      throw Error(`Key must start with prefix: "${this.prefix}".`);
+
+    const index = key.replace(this.prefix, "");
+    const parsedIndex = parseInt(index);
+
+    if (isNaN(parsedIndex) || parsedIndex < 0)
+      throw Error("Key does not contain a valid index.");
+
+    return parsedIndex;
   }
 }
